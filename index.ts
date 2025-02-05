4d8a9e3a6e9239725d5c64332781a968fc03463d
spectralo <arthur@spectralo.me>
2025-02-05 20:00:27 +0100

    fixed cors!

--- index.ts
+++ index.ts
@@ -1,3 +1,35 @@
+import axios from "axios";
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const file_path = "/tokens.json";
+const PORT = process.env.PORT;
+
+async function addToken(newToken: string, newJuiceToken: string) {
+  try {
+    const file = Bun.file(file_path);
+    const jsonData = await file.json();
+
+    jsonData[newToken] = newJuiceToken;
+
+    await Bun.write(file_path, JSON.stringify(jsonData, null, 2));
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+async function removeToken(tokenToRemove: string) {
+  try {
+    const file = Bun.file(file_path);
+    const jsonData = await file.json();
+    delete jsonData[tokenToRemove];
+    await Bun.write(file_path, JSON.stringify(jsonData, null, 2));
+    console.log("Token removed successfully.");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 Bun.serve({
   port: PORT,
   fetch(req) {
@@ -14,7 +46,6 @@ Bun.serve({
         },
       });
     }
-
     if (url.pathname === "/add") {
       const code = url.searchParams.get("code");
       const juicetoken = url.searchParams.get("juice");
@@ -32,7 +63,6 @@ Bun.serve({
         console.log("failed : /");
       }
     }
-
     if (url.pathname === "/remove") {
       const code = url.searchParams.get("code");
       try {
@@ -49,12 +79,6 @@ Bun.serve({
         console.log("failed : /");
       }
     }
-
-    return new Response("JuiceStats!", {
-      headers: {
-        "Access-Control-Allow-Origin": "*", // Allow any origin
-        "Content-Type": "text/plain",
-      },
-    });
+    return new Response("JuiceStats!");
   },
 });
